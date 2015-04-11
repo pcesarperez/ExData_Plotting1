9118a3e01fed0d09d81d6b22d8df69d21dd68261
pcesarperez <pcesarperez@gmail.com>
2015-04-11 17:14:03 +0100

    The creation of the filtered dataset is now slightly different.
    In order to get a clean dataset with proper dates and times, we need to perform some conversion. In this case, we create a new column (`Date_time`) which holds the date/time of each observation in POSIXct format.

--- preload.R
+++ preload.R
@@ -11,21 +11,32 @@
 # 	`DATA_OUT_FILE`: Filename of the filtered data subset.
 # 	`SEPARATOR`: Character used to separate data in the main file.
 # 	`FILTER_QUERY`: Query used to subset the data to work with.
+# 	`DATE_TIME_FORMAT`: Format of the date/time column we need to create in the data frame.
+# 	`TIME_ZONE`: Empty string to represent the current time zone.
 DATA_IN_FILE <- "household_power_consumption.txt"
 DATA_OUT_FILE <- "household_power_consumption_filtered.txt"
 SEPARATOR <- ";"
 FILTER_QUERY <- "select * from file where ((Date = '1/2/2007') or (Date = '2/2/2007'))"
+DATE_TIME_FORMAT <- "%d/%m/%Y %H:%M:%S"
+TIME_ZONE <- ""
 
-# The package `sqldf` must be installed.
+# The package `sqldf` must be installed to filter the data while reading.
 require (sqldf)
 
 # We are going to extract the data corresponding to these dates: 01/02/2007 and 02/02/2007.
 print ("Step 1: reading and filtering data file...")
-data <- read.csv.sql (DATA_IN_FILE, sql = FILTER_QUERY, header = TRUE, sep = SEPARATOR)
+data <- read.csv.sql (DATA_IN_FILE, sql = FILTER_QUERY, header = TRUE, sep = SEPARATOR, stringsAsFactors = FALSE)
 
-# Let's write it to a new file.
+# We are going to add a new column with the date/time of each observation.
+# We also remove the date and time columns, as we don't need them any more.
+print ("Step 2: creating a new date/time column...")
+data$Date_time <- as.POSIXct (paste (data$Date, data$Time), tz = TIME_ZONE, DATE_TIME_FORMAT)
+data$Date <- NULL
+data$Time <- NULL
+
+# Let's write the filtered data frame to a new file.
 # This is a one-time process; once finished, the filtered dataset will contain the data to plot.
-print ("Step 2: writing filtered data subset...")
+print ("Step 3: writing filtered data subset...")
 connection = file (DATA_OUT_FILE, "w")
 write.table (data, connection, quote = FALSE, row.names = FALSE, sep = SEPARATOR)
 close (connection)
